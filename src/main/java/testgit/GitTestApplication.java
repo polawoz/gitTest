abf012bd3716f2f242e18fba3f11f81ed6875279
Pola Wozniakowska <pola.wozniakowska@capgemini.com>
2020-08-03 21:03:47 +0200

    first commit

---
+++ src/main/java/testgit/GitTestApplication.java
@@ -0,0 +1,13 @@
+package testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "hsql");
+        SpringApplication.run(GitTestApplication.class, args);
+    }
+}
