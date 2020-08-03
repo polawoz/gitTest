01315fdd945c1958b2de33b8deb72b0ec778956d
yonasti <prawinski@edu.cdv.pl>
2020-08-03 21:40:17 +0200

    kochana Prosiaczka - komentarz

--- src/main/java/testgit/GitTestApplication.java
+++ src/main/java/testgit/GitTestApplication.java
@@ -9,5 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "hsql");
         SpringApplication.run(GitTestApplication.class, args);
+
+        System.out.println("Prrrrrroooooosiaczka lovvvvve");
     }
 }
